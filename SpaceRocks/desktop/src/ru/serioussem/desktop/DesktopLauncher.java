6e38e25a9eade0d9e9dfdd8209429f8140b88bff
VertDaider <serioussem92@gmail.com>
2021-01-29 11:18:50 +0700

    first commit

---
+++ SpaceRocks/desktop/src/ru/serioussem/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.serioussem.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.serioussem.SpaceRocks;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceRocks(), config);
+	}
+}
