84972f424cab4ddb3156e8b5cbe95c9485da1863
VertDaider <serioussem92@gmail.com>
2021-02-01 14:03:38 +0700

    start space rocks

---
+++ SpaceRocks/core/src/ru/serioussem/screens/BaseScreen.java
@@ -0,0 +1,105 @@
+package ru.serioussem.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public abstract class BaseScreen implements Screen, InputProcessor {
+    protected Stage mainStage;
+    protected Stage uiStage;
+
+    public BaseScreen() {
+        mainStage = new Stage();
+        uiStage = new Stage();
+
+        initialize();
+    }
+
+    public abstract void initialize();
+
+    public abstract void update(float dt);
+
+    public void render(float dt) {
+        uiStage.act(dt);
+        mainStage.act(dt);
+
+        update(dt);
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        mainStage.draw();
+        uiStage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    public void pause() {
+    }
+
+    public void resume() {
+    }
+
+    public void dispose() {
+    }
+
+    public void show() {
+        InputMultiplexer im = (InputMultiplexer) Gdx.input.getInputProcessor();
+        im.addProcessor(this);
+        im.addProcessor(uiStage);
+        im.addProcessor(mainStage);
+    }
+
+    public void hide() {
+        InputMultiplexer im = (InputMultiplexer) Gdx.input.getInputProcessor();
+        im.removeProcessor(this);
+        im.removeProcessor(uiStage);
+        im.removeProcessor(mainStage);
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
--- SpaceRocks/desktop/src/ru/serioussem/desktop/DesktopLauncher.java
+++ SpaceRocks/desktop/src/ru/serioussem/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import ru.serioussem.SpaceRocks;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1200;
+		config.height = 900;
+		config.samples=3;
+//		config.fullscreen = true;
+		config.title = "Space Rocks";
 		new LwjglApplication(new SpaceRocks(), config);
 	}
 }
